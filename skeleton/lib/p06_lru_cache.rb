09fe78816fff7104b878e34a5630ac0858b7c606
App Academy Student <student@appacademy.io>
2016-09-23 17:37:04 -0700

    half-finish lru_cache

--- skeleton/lib/p06_lru_cache.rb
+++ skeleton/lib/p06_lru_cache.rb
@@ -15,6 +15,14 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      @map.get(key)
+    else
+      val = calc!(key)
+      if count == @max
+        eject!
+        @store.insert(key, val)
+    end
   end
 
   def to_s
@@ -25,12 +33,18 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    @map.set(key, val)
+    val
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+
   end
 
   def eject!
+    @store.delete(@store.first.key)
+    @map.delete(@)
   end
 end
